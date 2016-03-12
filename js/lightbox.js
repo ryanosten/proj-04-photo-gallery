5ed3a827ed8cc3dc98797179bfebd0b962f7f0b5
Ryan Osten <ostenrj@gmail.com>
2016-03-12 12:58:52 -0500

    fixed first and last image cycling bug
    bug where when user cycled to first or last image, then tried to go
    back, image wouldn’t update.

--- js/lightbox.js
+++ js/lightbox.js
@@ -19,17 +19,18 @@ $("body").append($overlay);
 
 //Capture click event and link clicked
 $(".container a").click(function(event){
+	
 	//prevent default behavior
 	event.preventDefault();
 	
 	//store thumbnail anchor that was clicked for cycling through pics with arrows
 	$imageSelected= $(event.target)
 	
-	//Capture the image location
-	imageLocation = $(this).attr("href")
+	//capture the image location
+	$imageLocation = $(this).attr("href")
 
 	//update the overlay-img with the image location
-	$image.children("#overlay-img").attr("src", imageLocation);
+	$image.children("#overlay-img").attr("src", $imageLocation);
 
 	//Capture the alt attribute of the element clicked
 	$captionClicked = $(this).children().attr("alt")
@@ -57,19 +58,20 @@ $overlay.click(function(event){
 
 //When arrow is clicked, we want to cycle through the images
 
-
 $("#arrow-left").click(function(){
+	
+	if(!$imageSelected.is("#first")) {
 	
 	//traverse up DOM to get previous .pics div
 	$prevImageDiv = $imageSelected.closest("div").prev();
 	
-	//grab the href from the anchor element
-	$prevImageLocation = $prevImageDiv.find("a").attr("href");
+	//update imageLocation with the href from the previous div anchor element  
+	$imageLocation = $prevImageDiv.find("a").attr("href");
 
 	//update the #overlay-img's src attribute
-	$image.find('#overlay-img').attr("src", $prevImageLocation);
+	$image.find('#overlay-img').attr("src", $imageLocation);
 
-	//update caption clicked to be caption of previous image
+	//update caption clicked to be alt attribute of previous image
 	$captionClicked = $prevImageDiv.find("img").attr("alt");
 
 	//update caption of overlay
@@ -77,22 +79,49 @@ $("#arrow-left").click(function(){
 	
 	//update imageSelected to be current image
 	$imageSelected = $prevImageDiv.find("a");
+	
+	}
 
 	return false;
 })
 
 $("#arrow-right").click(function(){
 	
+	if(!$imageSelected.is("#last")) {
+	
 	//traverse down DOM to get next .pics div
 	$nextImageDiv = $imageSelected.closest("div").next();
 	
-	//grab the href from the anchor element
-	$nextImageLocation = $nextImageDiv.find("a").attr("href");
+	//update imageLocation with the href from the next div anchor element  
+	$imageLocation = $nextImageDiv.find("a").attr("href");
 
 	//update the #overlay-img's src attribute
-	$image.find('#overlay-img').attr("src", $nextImageLocation);
+	$image.find('#overlay-img').attr("src", $imageLocation);
 
-	//update $captionclicked to be caption of previous image
+	//update caption clicked to be alt attribute of next image
+	$captionClicked = $nextImageDiv.find("img").attr("alt");
+
+	//update caption of overlay
+	$caption.text($captionClicked);
+	
+	//update imageSelected to be current image
+	$imageSelected = $nextImageDiv.find("a");
+	
+	}
+
+	return false;
+})
+
+/*
+function getImage() {
+
+	//update imageLocation with the href from the next div anchor element  
+	$imageLocation = $nextImageDiv.find("a").attr("href");
+
+	//update the #overlay-img's src attribute
+	$image.find('#overlay-img').attr("src", $imageLocation);
+
+	//update caption clicked to be alt attribute of next image
 	$captionClicked = $nextImageDiv.find("img").attr("alt");
 
 	//update caption of overlay
@@ -101,9 +130,8 @@ $("#arrow-right").click(function(){
 	//update imageSelected to be current image
 	$imageSelected = $nextImageDiv.find("a");
 
-	return false;
-})
-
+}
+*/
 
 
 
