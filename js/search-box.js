4c9f5ba028b3854dcb7fd7d892dd3d425a15da8b
Ryan Osten <ostenrj@gmail.com>
2016-03-19 21:31:22 -0400

    fixed search box

--- js/search-box.js
+++ js/search-box.js
@@ -1,17 +1,15 @@
 var $value = $('');
 var $altText = $('');
-var count = 0;
 
 
 //When user enters text in the input field, check the alt text on imgs and only show imgs that match input text
 $('.search').keyup(function() {
 	
-	//store the input text and store count of number of characters in input
-	$value = $(this).val().toLowerCase();
-	count = $value.length;
-	
 	//hide all images
 	$(".container img").addClass("hide");
+	
+	//store the input text and store count of number of characters in input
+	$value = $(this).val().toLowerCase();
 	
 	//only show images where $value matches the alt attribute value of img
 	$(".container img").each(function(){
@@ -21,7 +19,7 @@ $('.search').keyup(function() {
 
 		/*check if the alt text matches the search input value. Must use substring method 
 		to check for exact match at beginning of alt text, otherwise module will look for $value at any position in $altText*/
-		if($altText.substring(0, count) == $value){
+		if($altText.indexOf($value) != -1 ){
 			$(this).removeClass("hide");
 		} 
 	});
